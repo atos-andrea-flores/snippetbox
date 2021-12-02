2fcb450fc5a0f02d0895b352829f145d483a635b
Flores <andrea.flores@atos.net>
2021-12-02 13:02:06 -0600

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module handy.net/snippetbox
+
+go 1.17
---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+//Define a home handler function which writes a byte slice containing
+// "Hello from Snippetbox" as the response body.
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from SnippetBox Home Handler"))
+}
+
+func main() {
+	//Use the http.NewServerMux funtion to initialize a new servermux, then
+	//register the home function as the handler for the "/" URL pattern-
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+
+	// Use the http.ListenAndServe() function to start a new web server. We pass
+	// two parameters: the TCP network address to listen on (in this case ":4000)
+	// and the servemux we just created. If http.ListenAndServe() returns an error
+	// we use the log.Fatal() function to log the error message and exit.
+	log.Println("Starting server on:4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
+
+}
